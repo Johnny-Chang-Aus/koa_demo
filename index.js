e00bccf42004ce09c5cae741d2d0c86139419878
Johnny Chang <johnnychang358@gmail.com>
2020-09-30 16:23:43 +1000

    update index.js

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const Koa = require('koa');
 
 const app = new Koa(); //函數第一個字母一定要大寫
 
-//以下為洋蔥模型, 以三層為例
+//以下為洋蔥模型(onion model), 以三層為例
 app.use(async (ctx, next) => {
     //left side of onion
     console.log('mid1 left', ctx.request.url);
-    ctx.time = Date.now(); //起始時間
+    ctx.time = Date.now(); //[寫法a]起始時間
+    const time = Date.now(); //[寫法b]起始時間
     await next(); //利用這個中間鍵統計所有流程的執行時間
     //right side of onion
     console.log('mid1 right', ctx.response.body);
-    console.log(Date.now() - ctx.time); //結束時間,減去起始時間則能得出所有流程的時間
+    console.log(Date.now() - ctx.time); //[寫法a]結束時間,減去起始時間則能得出所有流程的時間
+    const rt = Date.now() - time; //[寫法b] rt為response time
+    ctx.set('x-rt', rt); //[寫法b] 回傳response time
 });
 
 app.use(async (ctx, next) => {
